04d6eaa6ac8f086758f43e96646622cacf4c4c27
JordanHaer <jordanhaer@gmail.com>
2023-06-08 00:12:46 +0100

    removed rounded border on blog card

--- src/app/componets/BlogList.tsx
+++ src/app/componets/BlogList.tsx
@@ -17,7 +17,7 @@ export default function BlogList({ posts }: BlogListProps) {
       <div className="container flex-grow max-w-5xl mx-auto px-4 py-8 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {posts.map((post) => (
           <Link key={post.id} href={`/blog/${post.slug}`}>
-            <div className="bg-white rounded-lg shadow-md p-6 h-full hover:bg-gray-100 transition-colors flex flex-col justify-between">
+            <div className="bg-white shadow-md p-6 h-full hover:bg-gray-100 transition-colors flex flex-col justify-between">
               <p className="text-2xl font-bold text-blue-500 mb-8">{post.title}</p>
               <p className="mt-2 line-clamp-3">{post.content}</p>
             </div>
