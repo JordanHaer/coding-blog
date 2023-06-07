1564e7947a5c9658d34605e0c5275c0c072442fa
JordanHaer <jordanhaer@gmail.com>
2023-06-08 00:13:11 +0100

    added hero image

--- src/app/componets/Hero.tsx
+++ src/app/componets/Hero.tsx
@@ -1,25 +1,36 @@
 import Link from "next/link";
 import React from "react";
+import hero from "../../../public/images/hero.jpg";
 
 export default function Hero() {
   return (
-    <main className="flex-grow sm:px-2">
-      <div className="container mx-auto px-4 py-12">
-        <div className="max-w-3xl mx-auto">
-          <h1 className="text-4xl font-bold text-gray-800 mb-6">Welcome to My Blog</h1>
-          <p className="text-gray-700 leading-loose">
-            Welcome to my coding blog, where I document my journey of learning and exploring web development. Here,
-            I share insights, tutorials, and experiences as I delve into the world of web technologies. Join me on
-            this exciting adventure as we uncover the intricacies of HTML, CSS, JavaScript, frameworks, and more.
-            Let&#39;s learn and grow together in the vast landscape of web development.
-          </p>
-          <div className="mt-6">
-            <Link
-              href="/blog"
-              className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded"
-            >
-              See Blog Posts
-            </Link>
+    <main className="flex-grow flex flex-col items-center">
+      <div
+        className="w-full relative"
+        style={{
+          backgroundImage: `url(${hero.src})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-50 " style={{ backdropFilter: "blur(8px)" }}></div>
+        <div className="container mx-auto px-4 py-12 relative z-10">
+          <div className="max-w-3xl mx-auto">
+            <h1 className="text-4xl font-bold text-white mb-6">Welcome to My Blog</h1>
+            <p className=" text-white leading-loose">
+              Welcome to my coding blog, where I document my journey of learning and exploring web development.
+              Here, I share insights, tutorials, and experiences as I delve into the world of web technologies.
+              Join me on this exciting adventure as we uncover the intricacies of HTML, CSS, JavaScript,
+              frameworks, and more. Let&#39;s learn and grow together in the vast landscape of web development.
+            </p>
+            <div className="mt-6">
+              <Link
+                href="/blog"
+                className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded"
+              >
+                See Blog Posts
+              </Link>
+            </div>
           </div>
         </div>
       </div>
