e2441b27c0903e86ab74149073816450e86c7639
JordanHaer <jordanhaer@gmail.com>
2023-06-08 00:13:34 +0100

    added mobile dropdown menu

--- src/app/componets/Header.tsx
+++ src/app/componets/Header.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import HamburgerIcon from "./HamburgerIcon";
+import CrossIcon from "./CrossIcon";
+
+const navLinks = [
+  { label: "Home", href: "/" },
+  { label: "Blog", href: "/blog" },
+  { label: "About", href: "/about" },
+];
 
 export default function Header() {
-  const navLinks = [
-    { href: "/", label: "Home" },
-    { href: "/", label: "About" },
-    { href: "/", label: "Contact" },
-  ];
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <header className="bg-white shadow">
       <div className="container mx-auto px-4 py-6">
@@ -15,7 +26,7 @@ export default function Header() {
               Jordan&apos;s Blog
             </Link>
           </div>
-          <div>
+          <div className="hidden md:block">
             {navLinks.map((link, index) => (
               <Link
                 key={index}
@@ -25,6 +36,24 @@ export default function Header() {
                 {link.label}
               </Link>
             ))}
+          </div>
+          <div className="md:hidden">
+            <button className="text-gray-600 hover:text-gray-800 focus:outline-none" onClick={toggleMenu}>
+              {isMenuOpen ? <CrossIcon /> : <HamburgerIcon />}
+            </button>
+            {isMenuOpen && (
+              <div className="absolute top-14 right-5 bg-gray-100 text-base z-50 list-none divide-y divide-gray-100 rounded shadow">
+                <ul className="py-1" aria-labelledby="dropdown">
+                  {navLinks.map((link, index) => (
+                    <li key={index}>
+                      <Link href={link.href} className="text-lg hover:bg-gray-200 text-gray-700 block px-4 py-2">
+                        {link.label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
         </nav>
       </div>
