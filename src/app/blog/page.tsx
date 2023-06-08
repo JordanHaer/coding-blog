96ef8a5ceef3b541586c8368d88e1107f3bf5b03
JordanHaer <jordanhaer@gmail.com>
2023-06-08 15:16:24 +0100

    added date created to blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,9 +2,11 @@ import BlogList from "../componets/BlogList";
 import Footer from "../componets/Footer";
 import Header from "../componets/Header";
 import { getPosts } from "@/lib/posts";
+import { compareAsc} from 'date-fns'
+
 
 export default async function BlogListPage() {
-  const posts = await getPosts();
+  const posts = await getPosts().sort((a, b) => compareAsc(new Date(a.date), new Date(b.date)))
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Header />
