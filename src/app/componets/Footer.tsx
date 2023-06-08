0e8f9e3753c7cc6490db99347ec327e7072fb6b4
JordanHaer <jordanhaer@gmail.com>
2023-06-08 09:43:41 +0100

    added social icons

--- src/app/componets/Footer.tsx
+++ src/app/componets/Footer.tsx
@@ -1,11 +1,22 @@
+import GitHubIcon from "./GitHubIcon";
+import LinkedInIcon from "./LinkedInIcon";
+
 export default function Footer() {
   return (
     <footer className="bg-gray-200 py-4">
-        <div className="container mx-auto px-4">
-          <p className="text-center text-gray-600">
-            &copy; {new Date().getFullYear()} Jordan Haer. All rights reserved.
-          </p>
-        </div>
-      </footer>
-  )
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        <p className="text-center text-gray-600">
+          &copy; {new Date().getFullYear()} Jordan Haer. All rights reserved.
+        </p>
+        <ul className="flex items-center gap-4">
+          <li>
+            <LinkedInIcon />
+          </li>
+          <li>
+            <GitHubIcon />
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
--- src/app/componets/HamburgerIcon.tsx
+++ src/app/componets/HamburgerIcon.tsx
@@ -1,13 +1,5 @@
 export default function HamburgerIcon() {
   return (
-    <svg
-      className="w-6 h-6"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" height="1.25rem" viewBox="0 0 448 448"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
   );
 }
