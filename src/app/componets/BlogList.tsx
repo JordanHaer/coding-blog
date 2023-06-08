4c7ef69ae95c03e2b9f3dc16b550278c496bb02c
JordanHaer <jordanhaer@gmail.com>
2023-06-08 15:08:47 +0100

    added date created to blog posts

--- src/app/componets/BlogList.tsx
+++ src/app/componets/BlogList.tsx
@@ -1,25 +1,23 @@
 import Link from "next/link";
-
-interface BlogPost {
-  id: number;
-  slug: string;
-  title: string;
-  content: string;
-}
+import { Post } from "contentlayer/generated";
+import { format, parseISO } from "date-fns";
 
 interface BlogListProps {
-  posts: BlogPost[];
+  posts: Post[];
 }
 
 export default function BlogList({ posts }: BlogListProps) {
   return (
     <main className="flex-grow px-4 sm:px-4">
       <div className="container flex-grow max-w-5xl mx-auto px-4 py-8 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {posts.map((post) => (
-          <Link key={post.id} href={`/blog/${post.slug}`}>
-            <div className="bg-white shadow-md p-6 h-full hover:bg-gray-100 transition-colors flex flex-col justify-between">
-              <p className="text-2xl font-bold text-blue-500 mb-8">{post.title}</p>
-              <p className="mt-2 line-clamp-3">{post.content}</p>
+        {posts.map((post: Post) => (
+          <Link key={post.slug} href={`/blog/${post.slug}`}>
+            <div className="bg-white shadow-md p-6 h-full hover:bg-gray-100 transition-colors flex flex-col justify-between gap-16">
+              <div>
+                <p className="text-2xl font-bold text-blue-500 mb-4">{post.title}</p>
+                <div className="content" dangerouslySetInnerHTML={{ __html: post.body.html }}></div>
+              </div>
+              <p className="line-clamp-3 text-gray-400 ">{format(parseISO(post.date), "LLLL d, yyyy")}</p>
             </div>
           </Link>
         ))}
--- src/app/componets/BlogPost.tsx
+++ src/app/componets/BlogPost.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
-import React from "react";
+import { Post } from "contentlayer/generated";
+import { format, parseISO } from "date-fns";
 
-interface Props {
-  post: { id: number; slug: string; title: string; content: string };
+interface BlogPostProps {
+  post: Post;
 }
 
-export default function BlogPost({ post }: Props) {
+export default function BlogPost({ post }: BlogPostProps) {
   return (
     <main className="flex-grow sm:px-2">
       <div className="container mx-auto px-4 py-12">
         <div className="max-w-3xl mx-auto">
-          <h1 className="text-4xl font-bold text-gray-800 mb-6">{post.title}</h1>
-          <p className="text-gray-700 leading-loose">{post.content}</p>
+          <h1 className="text-4xl font-bold text-gray-800 mb-3">{post.title}</h1>
+          <p className="text-gray-400 mb-3">{format(parseISO(post.date), "LLLL d, yyyy")}</p>
+          <div className="prose max-w-none" dangerouslySetInnerHTML={{ __html: post.body.html }}></div>
           <div className="mt-6">
             <Link
               href="/blog"
