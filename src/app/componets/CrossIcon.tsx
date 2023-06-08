e646fb4515c06119ba1ddb1ca8ff42f781c52d8a
JordanHaer <jordanhaer@gmail.com>
2023-06-08 09:44:49 +0100

    changed cross svg

--- src/app/componets/CrossIcon.tsx
+++ src/app/componets/CrossIcon.tsx
@@ -1,13 +1,7 @@
 export default function CrossIcon() {
   return (
-    <svg
-  className="w-6 h-6"
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="currentColor"
->
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-</svg>
+    <svg xmlns="http://www.w3.org/2000/svg" height="1.25rem" viewBox="0 0 512 512">
+      <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"></path>
+    </svg>
   );
 }
