f2b2b914a4f04d67f36f0b2fc7bb36ec0ae9c2a4
JordanHaer <jordanhaer@gmail.com>
2023-06-08 22:57:49 +0100

    Styling tweak

--- src/app/componets/BlogPost.tsx
+++ src/app/componets/BlogPost.tsx
@@ -8,8 +8,8 @@ interface BlogPostProps {
 
 export default function BlogPost({ post }: BlogPostProps) {
   return (
-    <main className="flex-grow sm:px-2">
-      <div className="container mx-auto px-4 py-12">
+    <main className="flex-grow">
+      <div className="container mx-auto px-4 py-6 sm:py-12">
         <div className="max-w-3xl mx-auto">
           <h1 className="text-4xl font-bold text-gray-800 mb-3">{post.title}</h1>
           <p className="text-gray-400 mb-3">{format(parseISO(post.date), "LLLL d, yyyy")}</p>
