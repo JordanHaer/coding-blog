6d7814a3c1c2a9999b37f8070f92d2c0fa655823
JordanHaer <jordanhaer@gmail.com>
2023-06-09 13:52:25 +0100

    Changes to content

--- src/app/componets/AboutBody.tsx
+++ src/app/componets/AboutBody.tsx
@@ -6,26 +6,27 @@ export default function AboutBody() {
     <main className="flex-grow flex flex-col items-center">
       <div className="container mx-auto px-4 py-12">
         <div className="max-w-3xl mx-auto">
-          <h1 className="text-4xl font-bold mb-6">About</h1>
+          <h1 className="text-4xl font-bold mb-6">About Me</h1>
           <p className="leading-loose mb-6">
-            Welcome to my coding blog! My name is Jordan, and I&#39;m passionate about all things coding and
-            technology. This blog is my way of sharing my knowledge, experiences, and insights with fellow coding
-            enthusiasts.
+            I&#39;m Jordan, and I&#39;m thrilled to share my journey of learning to code
+            and my experiences as a developer with you. This blog is a platform where I can document my progress,
+            share insights, and showcase my projects.
           </p>
           <p className="leading-loose mb-6">
-            Throughout this blog, you&#39;ll find a variety of articles, tutorials, and resources covering topics
-            such as web development, programming languages, software engineering, and more. My goal is to provide
-            valuable content that helps you enhance your coding skills and stay updated with the latest trends in
-            the industry.
+            As you explore this blog, you&#39;ll discover a variety of articles and resources that cover various
+            aspects of web development, programming languages, software engineering, and more. My aim is to provide
+            valuable content that not only helps you enhance your coding skills but also offers glimpses into my
+            personal growth and development as a coder.
           </p>
           <p className="leading-loose mb-6">
-            Whether you&#39;re a beginner just starting your coding journey or an experienced developer looking to
-            expand your knowledge, I hope you&#39;ll find useful information and inspiration here. Feel free to
-            explore the blog and leave comments or questions on any post that interests you. I&#39;m always excited
-            to engage with the coding community!
+            Whether you&#39;re just starting out on your coding journey or have years of experience, I hope
+            you&#39;ll find inspiration and useful information here. I encourage you to dive into the blog, explore
+            different posts, and engage with the content by reaching out to me with any questions or thoughts you
+            may have. I&#39;m always open to discussions and eager to connect with fellow coding enthusiasts.
           </p>
           <p className="leading-loose mb-6">
-            Thank you for visiting my blog, and I hope you enjoy your time here. Happy coding!
+            Thank you for joining me on this exciting adventure of learning and sharing. I&#39;m grateful for your
+            visit to my blog, and I hope you enjoy your time here. Happy coding!
           </p>
           <AllBlogPostsButton />
         </div>
