5a2e123eda132d67a98ac767312696ee41df8c62
JordanHaer <jordanhaer@gmail.com>
2023-06-09 23:32:37 +0100

    changed hero image

--- src/app/componets/Hero.tsx
+++ src/app/componets/Hero.tsx
@@ -7,17 +7,17 @@ export default function Hero() {
   return (
     <div className="flex flex-col items-center">
       <div className="w-full relative">
-        <Image src={hero} alt="Hero Image" fill placeholder="blur" />
-        <div className="absolute inset-0 bg-black bg-opacity-50 " style={{ backdropFilter: "blur(8px)" }}></div>
+        <Image src={hero} alt="Hero Image" fill placeholder="blur" className="object-cover" />
+        <div className="absolute inset-0 bg-black opacity-60"></div>
         <div className="container mx-auto px-4 py-6 sm:py-12 relative z-10">
           <div className="max-w-3xl mx-auto">
-            <h1 className="text-4xl font-bold text-white mb-6">Welcome to My Blog!</h1>
+            <h1 className="text-4xl font-bold text-white mb-6 leading-snug">Welcome to My Blog!</h1>
             <p className=" text-white leading-loose mb-6">
-              Here, I document my journey of learning to code and making strides in the tech
-              industry. This platform serves as an outlet for me to share the knowledge and insights I&#39;ve
-              gained throughout my experiences. As I explore new technologies, languages, and frameworks, I will
-              document my encounters and provide updates on the projects I&#39;m currently working on. Stay tuned
-              for exciting content and glimpses into my coding endeavors!
+              Here, I document my journey of learning to code and making strides in the tech industry. This
+              platform serves as an outlet for me to share the knowledge and insights I&#39;ve gained throughout my
+              experiences. As I explore new technologies, languages, and frameworks, I will document my encounters
+              and provide updates on the projects I&#39;m currently working on. Stay tuned for exciting content and
+              glimpses into my coding endeavors!
             </p>
             <AllBlogPostsButton />
           </div>
