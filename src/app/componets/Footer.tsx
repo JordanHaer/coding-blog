e52bda43e09a0a9c146e0ac80c2faea8d10fbd54
JordanHaer <jordanhaer@gmail.com>
2023-06-10 23:06:35 +0100

    adding gmail icon

--- src/app/componets/Footer.tsx
+++ src/app/componets/Footer.tsx
@@ -1,5 +1,12 @@
 import GitHubIcon from "../icons/GitHubIcon";
+import GmailIcon from "../icons/GmailIcon";
 import LinkedInIcon from "../icons/LinkedInIcon";
+
+const footerIcons = [
+  { link: "https://github.com/JordanHaer", icon: <GitHubIcon /> },
+  { link: "https://linkedin.com/in/JordanHaer", icon: <LinkedInIcon /> },
+  { link: "mailto:jordanhaer@gmail.com", icon: <GmailIcon /> },
+];
 
 export default function Footer() {
   return (
@@ -8,17 +15,14 @@ export default function Footer() {
         <p className="text-center text-gray-600">
           &copy; {new Date().getFullYear()} Jordan Haer. All rights reserved.
         </p>
-        <ul className=" items-center gap-4 hidden  sm:flex">
-          <li>
-            <a href="https://github.com/JordanHaer" target="_blank" rel="noopener noreferrer">
-              <GitHubIcon />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/JordanHaer" target="_blank" rel="noopener noreferrer">
-              <LinkedInIcon />
-            </a>
-          </li>
+        <ul className="items-center gap-4 hidden sm:flex">
+          {footerIcons.map(({ link, icon }, index) => (
+            <li key={link}>
+              <a href={link} target="_blank" rel="noopener noreferrer">
+                {icon}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </footer>
